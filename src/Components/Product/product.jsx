b7174c23a4972d7decfc989d12311ea7e77d5f08
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-03-30 13:35:53 +0600

    icons addded and favicon added cart added

--- src/Components/Product/product.jsx
+++ src/Components/Product/product.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./product.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
+   
 	const { id, img, name, price, seller, quantity, ratting } = props.product;
+   const handleAddToCart=props.handleAddToCart
 	return (
 		<div className="product">
 			<img src={img} alt="" />
@@ -12,7 +16,12 @@ const Product = (props) => {
 				<p>Manufacture:{seller}</p>
 				<p>Ratting:{ratting} star</p>
 			</div>
-            <button className="btn-cart">Add To Cart</button>
+            <button  onClick={()=>
+           handleAddToCart(props.product)
+            }
+            className="btn-cart">Add To Cart
+             <FontAwesomeIcon icon={faShoppingCart} />
+            </button>
 		</div>
 	);
 };
