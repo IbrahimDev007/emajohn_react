d919a76e0443cc6226dbcb7fa1a9faac3f80bbc2
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-03-31 10:13:03 +0600

    card clculation finish

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -34,7 +34,21 @@ setCart(addSaveCart);
     
     const handleAddToCart=(product)=>{
         // console.log(id);
-        const newCart=[...cart,product]
+        // const newCart=[...cart,product]
+        let newCart=[];
+        const exist=cart.find(pd=>pd.id==product.id)
+        if(!exist){
+             product.quantity=1
+            newCart=[...cart,product] 
+            console.log(newCart);
+ 
+        }
+
+        else{
+ exist.quantity=exist.quantity+1
+const remaining =cart.filter(pd=>pd.id!==product.id)
+     newCart=[...remaining,exist]
+        }
         setCart(newCart)
         addToDb(product.id)
      }
